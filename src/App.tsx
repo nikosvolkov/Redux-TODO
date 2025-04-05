e463fb8d82d9711d8b7ae8533cf2cfda9de8c21f
Volkov Nikita <crystalnik123@gmail.com>
2025-04-05 18:23:21 +0300

    feat: finished todo app in react

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,28 @@ interface Todo {
   text: string;
 }
 
+type inputEvent = React.ChangeEvent<HTMLInputElement>;
+type formEvent = React.SyntheticEvent<HTMLFormElement>;
+
+const TODOS = [
+  { id: Date.now() + 1, text: 'React' },
+  { id: Date.now() + 2, text: 'JavaScript' },
+  { id: Date.now() + 3, text: 'Redux' },
+];
+
 function App() {
-  const [todos, setTodos] = useState<Todo[]>([]);
+  const [todos, setTodos] = useState<Todo[]>(TODOS);
   const [text, setText] = useState<string>('');
 
   const createTodo = () => {
+    if (!text) return;
     setTodos([...todos, { id: Date.now(), text }]);
-    setText('')
+    setText('');
   };
 
-  const deleteTodo = (id:number)=>{
-    setTodos(todos.filter(todo=>todo.id !== id))
-  } 
+  const deleteTodo = (id: number) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
 
   return (
     <>
@@ -25,27 +35,37 @@ function App() {
         <h1>Todo List using React+Redux+Typescript</h1>
       </header>
       <main>
-        <form action="" onSubmit={(e) => e.preventDefault()}>
+        <form action="" onSubmit={(e: formEvent) => e.preventDefault()}>
           <input
-          className='formInput'
+            className="formInput"
             type="text"
             placeholder="Текст задачи"
             value={text}
-            onChange={(e) => setText(e.target.value)}
+            onChange={(e: inputEvent) => setText(e.target.value)}
           />
 
-          <button className='formButton' onClick={createTodo}>Создать</button>
+          <button className="formButton" onClick={createTodo}>
+            Создать
+          </button>
         </form>
-
-        <ul className="list">
-          {todos.map((todo) => (
-            <li className="listItem">
-              <input type="checkbox" className='listCheckbox'/>
-              <p>{todo.text}</p>
-              <button className="listDelete" onClick={()=>deleteTodo(todo.id)}>X</button>
-            </li>
-          ))}
-        </ul>
+        {todos.length ? (
+          <ul className="list">
+            {todos.map((todo) => (
+              <li className="listItem" key={todo.id}>
+                <input type="checkbox" className="listCheckbox" />
+                <p>{todo.text}</p>
+                <button
+                  className="listDelete"
+                  onClick={() => deleteTodo(todo.id)}
+                >
+                  X
+                </button>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p>Список задач пуст</p>
+        )}
       </main>
     </>
   );
